fc0c631d9db8dba24c06995e9646331a1d0109f1
halil_consul <halil_consul@list.ru>
2017-01-11 21:20:24 +0300

    added redux

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, Route, hashHistory } from 'react-router';
+import store from './store/index.js';
 import App from './App.jsx';
 
 ReactDOM.render(
-   <App />,
+   <Provider store={store}>
+      <Router history={hashHistory}>
+         <Route path='/' component={App}>
+
+         </Route>
+      </Router>
+   </Provider>,
    document.getElementById('mount-point')
 );
---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,12 @@
+import AppConstants from '../constants/AppConstants.js';
+
+const initialState = {
+   isLoggedIn: false
+}
+
+export default function reducer(state=initialState, action) {
+   switch (action.type) {
+
+   }
+   return state;
+}
